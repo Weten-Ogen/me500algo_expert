48b7dbfc368ad3511e4b8046b175a06ff6425fbb
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-11-07 18:28:45 +0000

    sorting/insertion.py

---
+++ sorting/insertion.py
@@ -0,0 +1,17 @@
+"""
+    Insertion sort 
+    :array -> str ( return a sorted list)
+    
+"""
+def insertion(arr):
+    for j in range(1,len(arr)):
+        key = arr[j]
+        i = j + 1
+        while i >= 0 and arr[i] > key:
+            arr[i + 1] = arr[i]
+            i -= 1
+        arr[i + 1] = key
+    return arr
+
+arr = [55,43,3,2,7,4,2,1,18,45,13]
+print(insertion(arr))
